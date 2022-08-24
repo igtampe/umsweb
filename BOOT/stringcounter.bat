568e27bd39d2e44bdde88a1cca337e9136988dd4
Ignacio Tampe (Chopo) <Igtampe@gmail.com>
2022-08-23 22:47:43 -0400

    Half Boot

---
+++ BOOT/stringcounter.bat
@@ -0,0 +1,22 @@
+@echo off
+set "myString=%1"
+call :strlen result myString
+SET LENGTH=%RESULT%
+goto :eof
+
+:strlen <resultVar> <stringVar>
+(   
+    set "s=!%~2!#"
+    set "len=0"
+    for %%P in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
+        if "!s:~%%P,1!" NEQ "" ( 
+            set /a "len+=%%P"
+            set "s=!s:~%%P!"
+        )
+    )
+)
+( 
+    endlocal
+    set "%~1=%len%"
+    exit /b
+)
